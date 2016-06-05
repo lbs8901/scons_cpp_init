2731b2ca8bb1af58c6047469739d56fd892d2ff0
lee byungsoo <lbs8902@gmail.com>
2016-06-05 12:05:40 +0900

    first commit

---
+++ src/toolkit/toolkit.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "toolkit.h"
+#include "util.h"
+
+void toolkit_func() {
+	printf("toolkit func, x = %d\n", util_func());
+}
---
+++ test/someTests/tests.h
@@ -0,0 +1,17 @@
+
+#ifndef TESTS_H
+#define TESTS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Just calls a function in toolkit.h */
+int runTests();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
